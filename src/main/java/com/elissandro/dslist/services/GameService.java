a2eb742997320f62d1a66314460f00b9400aa943
elissandroa <elissandroa@hotmail.com>
2024-11-11 19:16:53 -0300

    Game repository, service, controller

---
+++ src/main/java/com/elissandro/dslist/services/GameService.java
@@ -0,0 +1,21 @@
+package com.elissandro.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.elissandro.dslist.dto.GameMinDTO;
+import com.elissandro.dslist.entities.Game;
+import com.elissandro.dslist.repositories.GameRepository;
+
+@Service
+public class GameService {
+	@Autowired
+	private GameRepository gameRepository;
+
+	public List<GameMinDTO> findAll() {
+		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+}
