b664bd086eb23b28d2fe7db9e20099f746d4efb4
elissandroa <elissandroa@hotmail.com>
2024-11-12 12:56:49 -0300

    Projections

--- src/main/java/com/elissandro/dslist/dto/GameMinDTO.java
+++ src/main/java/com/elissandro/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.elissandro.dslist.dto;
 
 import com.elissandro.dslist.entities.Game;
+import com.elissandro.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 	private Long Id;
@@ -19,6 +20,14 @@ public class GameMinDTO {
 		year = entity.getYear();
 		imgUrl = entity.getImgUrl();
 		shortDescription = entity.getShortDescription();
+	}
+	
+	public GameMinDTO(GameMinProjection projection) {
+		Id = projection.getId(); 
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl =projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
 	}
 
 
