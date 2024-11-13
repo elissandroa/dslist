fc26e20d2e9ed57f2ffb169bb0be72d886f60dac
elissandroa <elissandroa@hotmail.com>
2024-11-13 15:42:52 -0300

    Homolog ok

--- src/main/java/com/elissandro/dslist/controllers/GameController.java
+++ src/main/java/com/elissandro/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.elissandro.dslist.dto.GameDTO;
 import com.elissandro.dslist.dto.GameMinDTO;
 import com.elissandro.dslist.services.GameService;
 
@@ -16,7 +18,12 @@ public class GameController {
 
 	@Autowired
 	private GameService gameService;
-
+	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id) {
+		GameDTO game = gameService.findByID(id);
+		return game;
+	}
 
 	@GetMapping
 	public List<GameMinDTO> findAll() {
